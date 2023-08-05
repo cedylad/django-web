821b2cee77d33a7f9157aeb4196403c7bb6cb594
cedylad <cedricanassi@gmail.com>
2023-08-05 17:22:12 +0200

    Partie views+routes url ok

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
