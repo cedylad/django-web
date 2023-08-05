1de1c7d79e866e31cd27ec9215869300b8c1fd26
cedylad <cedricanassi@gmail.com>
2023-08-05 18:01:58 +0200

    database migrations et view ok

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Listing
+
+
 def hello(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    bands = Band.objects.all()
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+        <p>Article :<p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+        </ul>
+""")
 
 
 def about(request):
