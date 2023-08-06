eebd5ce0c96f453209b893829416f342587a2ef1
cedylad <cedricanassi@gmail.com>
2023-08-06 21:46:02 +0200

    gestion des form delete crud ok

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -92,6 +92,19 @@ def band_change(request, id):
     return render(request,'listings/band_change.html', {'form': form})
 
 
+def band_delete(request, id):
+    band = Band.objects.get(id=id)  # nécessaire pour GET et pour POST
+
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        band.delete()
+        # rediriger vers la liste des groupes
+        return redirect('band-list')
+
+    # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+    return render(request,'listings/band_delete.html',{'band': band})
+
+
 def listing_create(request):
 
     if request.method == 'POST':
@@ -121,3 +134,16 @@ def listing_change(request, id):
     else:
         form = ListingForm(instance=listing)
     return render(request,'listings/listing_change.html', {'form': form})
+
+
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)  # nécessaire pour GET et pour POST
+
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        listing.delete()
+        # rediriger vers la liste des groupes
+        return redirect('listing-list')
+
+    # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+    return render(request,'listings/listing_delete.html',{'listing': listing})
